4eca8d8f9ef902c8f7b3ce8a3287ba7f34c74159
LeeTaeYoung <xodud5080@naver.com>
2025-10-16 16:00:24 +0900

    various relationship mapping

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+}
